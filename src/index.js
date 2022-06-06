dede870d48a97f7d716a87049c24b9da38a9a3fb
mahyarErshad <mahyar.ershad92@gmail.com>
2022-06-06 19:31:56 +0430

    app works properly

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
-import React from "react"
+import React , {useState} from "react"
 import { createRoot } from "react-dom/client"
 
 function App() {
+  const [size , setSize] = useState(16)
+  const [color , setColor] = useState("black")
+  const [likeCounter , setLikeCounter] = useState(0)
+
+  function handleSize(e) {
+    setSize(e.target.value)
+    }
+  function handleColor(e) {
+    setColor(e.target.value)
+    }
+    function handleButton(){
+      setColor("black");
+      setSize(16);
+    }
   return (
     <div className="grid-parent">
       <div className="header">
-        <h1>Welcome To Our App</h1>
-        <p>The current size is x and the current color is x.</p>
+        <h1>Welcome To Our App!!!</h1>
+        <p>The current size is {size} and the current color is {color}.</p>
         <p>
-          This page has been liked <strong>0</strong> times.
+          This page has been liked <strong>{likeCounter}</strong> times.
         </p>
       </div>
       <div className="sidebar">
-        <input type="text" />
-        <input type="text" />
-        <button>Make the text 20px and pink</button>
+        <input type="text" value={size} onChange={handleSize}/>
+        <input type="text" value={color} onChange={handleColor}/>
+        <button onClick={handleButton}>Make the text default</button>
       </div>
-      <div className="main-area">
+      <div className="main-area" style={{color , fontSize:`${size}px`}}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quod obcaecati dolore debitis amet aut, excepturi voluptas ratione quibusdam laboriosam, optio pariatur provident doloremque consequatur animi? Consequuntur, officiis. Explicabo, vel.</p>
       </div>
       <footer className="footer">
         <p>
-          This is the footer. <button>Make the text 30px but leave the color the same</button>
+          This is the footer. <button onClick={()=> setSize(30)}>Make the text 30px but leave the color the same</button>
         </p>
         <p>
-          <button>Like The Page</button>
+          <button onClick={()=>setLikeCounter(likeCounter + 1)}>Like The Page</button>
         </p>
       </footer>
     </div>
